8e2973b946937c7c13fef6a4872ed686c94ce6b1
silasps <silaspereiras@gmail.com>
2022-11-25 21:25:51 -0300

    exercicios w09

---
+++ lista.semana9/src/main/java/lista/semana9/ListaM2S9_1Application.java
@@ -0,0 +1,13 @@
+package lista.semana9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaM2S9_1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaM2S9_1Application.class, args);
+	}
+
+}
